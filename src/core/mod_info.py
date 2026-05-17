6040556901e89d31e04acaa252c84ee2870c0920
TheZerg <s.v.larichev@mail.com>
2026-05-18 00:39:46 +0700

    feat: update ModInfo structure and add file_hash attr

--- src/core/mod_info.py
+++ src/core/mod_info.py
@@ -13,17 +13,18 @@ class ModInfo:
 
     Attributes:
         data_source: Источник данных ("API", "Local", "Unknown").
-
-        mod_id: Внутренний идентификатор мода (id).
+        source_path: Полный путь к исходному файлу архива.
+        file_hash: Хэш файла для идентификации.
         name: Название модификации.
-        authors: Список авторов модификации.
+        description: Описание модификации.
+        loader_type: Тип загрузчика (Fabric, Quilt, Forge, NeoForge, Legacy Forge и т.д.).
         version: Версия модификации.
         minecraft_version: Версия Minecraft, для которой предназначен мод.
-        description: Описание модификации.
+
+        mod_id: Внутренний идентификатор мода (id).
+        authors: Список авторов модификации.
         icon_path: Путь к файлу иконки внутри архива.
-        source_path: Полный путь к исходному файлу архива.
         metadata_file_name: Имя найденного файла метаданных в архиве.
-        loader_type: Тип загрузчика (Fabric, Quilt, Forge, NeoForge, Legacy Forge).
 
         api_project_id: Уникальный ID проекта на Modrinth (например, 'P7dR8mSH').
         api_version_id: Уникальный ID конкретной версии файла на Modrinth.
@@ -34,22 +35,23 @@ class ModInfo:
         update_version: Номер новой версии (например, '1.2.4').
         update_changelog: Описание изменений (патчноут).
         update_download_url: Прямая ссылка на скачивание .jar файла новой версии.
-
         update_dependencies: Список зависимостей новой версии (зависимость {project_id: str, type: str}).
     """
+    # --- Общие данные ---
     data_source: str = "Unknown"
+    source_path: Optional[Path] = None
+    file_hash: Optional[str] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    loader_type: Optional[str] = None
+    version: Optional[str] = None
+    minecraft_version: Optional[str] = None
 
     # --- Локальные данные (Парсинг архива) ---
     mod_id: Optional[str] = None
-    name: Optional[str] = None
     authors: Optional[List[str]] = None
-    version: Optional[str] = None
-    minecraft_version: Optional[str] = None
-    description: Optional[str] = None
     icon_path: Optional[PurePosixPath] = None
-    source_path: Optional[Path] = None
     metadata_file_name: Optional[str] = None
-    loader_type: Optional[str] = None
     
     # --- Данные от API (Состояние текущего файла) ---
     api_project_id: Optional[str] = None
@@ -57,12 +59,11 @@ class ModInfo:
     api_icon_url: Optional[str] = None
     api_icon_data: Optional[bytes] = None
     
-    # --- Данные об обновлении (Целевая версия для скачивания) ---
+    # --- Данные от API об обновлении (Целевая версия для скачивания) ---
     update_filename: Optional[str] = None
     update_version: Optional[str] = None
     update_changelog: Optional[str] = None
     update_download_url: Optional[str] = None
-    
     # Зависимости мода (список словарей с project_id и типом зависимости)
     update_dependencies: List[Dict[str, Any]] = field(default_factory=list) 
 
